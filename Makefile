631280f27a64c7120af97465c13c81fff2b45abc
bassaer <app.nakayama@gmail.com>
2020-07-19 18:55:07 +0900

    init

---
+++ Makefile
@@ -0,0 +1,34 @@
+IMG       = os.img
+MAKE      = make --no-print-directory
+
+.PHONY: img run clean
+
+all: clean run
+
+kernel/kernel.bin:
+	@$(MAKE) build -C kernel
+
+boot/BOOTX64.EFI:
+	@$(MAKE) build -C boot
+
+img: boot/BOOTX64.EFI kernel/kernel.bin
+	dd if=/dev/zero of=$(IMG) bs=1k count=1440
+	mformat -i $(IMG) -f 1440 ::
+	mmd -i $(IMG) ::/EFI
+	mmd -i $(IMG) ::/EFI/BOOT
+	mcopy -i $(IMG) $< ::/EFI/BOOT
+	mcopy -i $(IMG) kernel/kernel.bin ::/
+
+run: img
+	qemu-system-x86_64 -name myos \
+                     -localtime \
+                     -monitor stdio \
+                     -bios /usr/share/ovmf/OVMF.fd \
+                     -net none \
+                     -usbdevice disk::$(IMG) \
+                     -d guest_errors \
+                     || true
+
+clean:
+	@$(MAKE) clean -C boot
+	@$(MAKE) clean -C kernel
---
+++ include/color.h
@@ -0,0 +1,48 @@
+#ifndef MYOS_COLOR_H
+#define MYOS_COLOR_H
+
+#define BLACK           0x00
+#define BLUE            0x01
+#define GREEN           0x02
+#define CYAN            0x03
+#define RED             0x04
+#define PURPLE          0x05
+#define BROWN           0x06
+#define GRAY            0x07
+#define DARKGRAY        0x08
+#define LIGHT_BLUE      0x09
+#define LIGHT_GREEN     0x0A
+#define LIGHT_CYAN      0x0B
+#define LIGHT_RED       0x0C
+#define LIGHT_PURPLE    0x0D
+#define YELLOW          0x0E
+#define WHITE           0x0F
+
+#define EFI_BLACK 0x00
+#define EFI_BLUE 0x01
+#define EFI_GREEN 0x02
+#define EFI_CYAN 0x03
+#define EFI_RED 0x04
+#define EFI_MAGENTA 0x05
+#define EFI_BROWN 0x06
+#define EFI_LIGHTGRAY 0x07
+#define EFI_BRIGHT 0x08
+#define EFI_DARKGRAY 0x08
+#define EFI_LIGHTBLUE 0x09
+#define EFI_LIGHTGREEN 0x0A
+#define EFI_LIGHTCYAN 0x0B
+#define EFI_LIGHTRED 0x0C
+
+#define EFI_LIGHTMAGENTA 0x0D
+#define EFI_YELLOW 0x0E
+#define EFI_WHITE 0x0F
+#define EFI_BACKGROUND_BLACK 0x00
+#define EFI_BACKGROUND_BLUE 0x10
+#define EFI_BACKGROUND_GREEN 0x20
+#define EFI_BACKGROUND_CYAN 0x30
+#define EFI_BACKGROUND_RED 0x40
+#define EFI_BACKGROUND_MAGENTA 0x50
+#define EFI_BACKGROUND_BROWN 0x60
+#define EFI_BACKGROUND_LIGHTGRAY 0x70
+
+#endif
